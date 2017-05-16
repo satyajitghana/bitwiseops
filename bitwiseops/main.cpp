3c631b284bf001de2159c5ef40d44ab445d13782
Satyajit Ghana <satyajitghana7@gmail.com>
2017-05-16 22:14:25 +0530

    Initial Commit

---
+++ bitwiseops/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bitwiseops
+//
+//  Created by Satyajit Ghana on 16/05/17.
+//  Copyright © 2017 Satyajit Ghana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
